baf008fc0fb870ce4b4bec46547dedd961b63b17
newbie-dev-freyl <softwaretransjp@gmail.com>
2024-07-19 05:51:24 +0800

    to solve at

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,99 +1,97 @@
-import './app.css'
+import React, { useEffect, useRef, useState } from 'react';
 
+const HeaderNavigation = () => {
+  const [isToggled, setIsToggled] = useState(false);
+  const [isDesktop, setIsDesktop] = useState(window.innerWidth > 843.20);
+  const navMenuRefs = useRef([]);
+  const navBackLinkRefs = useRef([]);
+  const toggleRef = useRef(null);
 
-const itemList = [
-    {
-        label: "Menu1"
-    },
+  const handleResize = () => {
+    setIsDesktop(window.innerWidth > 843.20);
+    setIsToggled(false);
+  };
 
-    {
-        label: "Menu2",
-        submenu: [
-            {
-                label: "Menu2",
-            },
-            {
-                label: "Submenu1",
-            },
-            {
-                label: "Submenu2"
-            }
-        ]
-    },
+  const toggleMenu = () => {
+    setIsToggled(!isToggled);
+    navMenuRefs.current.forEach(menu => {
+      menu.removeAttribute('toggled');
+    });
+  };
 
-    {
-        label: "Menu3",
-        submenu: [
-            {
-                label: "Menu3"
-            }, 
-            {
-                label: "Submenu31",
-                submenu: [
-                    {
-                        label: "Submenu31"
-                    },
-                    {
-                        label: "Sub Sub menu1"
-                    },
-                    {
-                        label: "Sub Sub menu2"
-                    },
-                    {
-                        label: "Sub Sub menu3"
-                    }
-
-                ]
-            },
-
-            {
-                label: "Submenu32"
-            }
-        ]
-    },
-]
-
-export default function App(){
-
-    function removeActive() {
-        const links = document.querySelectorAll('.nav__link')
-        links.forEach(el => {
-            el.classList.remove('active')
-        })
+  const handleLinkClick = (link, idx) => {
+    if (link.hasAttribute('toggle-menu')) {
+      navMenuRefs.current[idx].toggleAttribute('toggled');
+    }
+    if (link.hasAttribute('toggle-back')) {
+      navBackLinkRefs.current[idx].parentElement.parentElement.toggleAttribute('toggled');
     }
     
-    function toggle(e) {
-        console.log('clicked!')
-        let submenu = e.target.nextElementSibling
-        if (!submenu) return
-        e.target.classList.add('active')
-    }
+  };
 
-    function renderSubmenu(subMenu) {
-        return (
-            <ul className='nav__submenu' role='list'>
-                {subMenu.map((item, index) => (
-                    <li className='nav__subitem' key={index}>
-                            <a href="#"  className={`nav__sublink`} onClick={toggle}>{item.label}</a>
-                            {item.submenu && renderSubmenu(item.submenu)}
-                    </li>
-                ))}
+  useEffect(() => {
+    const handleAnimationDisable = () => {
+      document.documentElement.setAttribute('disabled-animation', '');
+      setTimeout(() => {
+        document.documentElement.removeAttribute('disabled-animation');
+      }, 100);
+    };
+
+    window.addEventListener('resize', handleAnimationDisable);
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleAnimationDisable);
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  useEffect(() => {
+    handleResize();
+  }, []);
+
+  return (
+    <div>
+      <button 
+        className="header-toggle" 
+        onClick={toggleMenu} 
+        ref={toggleRef} 
+        toggled={isToggled ? 'true' : undefined}
+      >
+        Toggle Menu
+      </button>
+      <nav className="header-navmenu">
+        <li>
+            <a href={`javascript:void(0)`} onClick={(e) => handleLinkClick(e.target, 0)} toggle-menu="true">
+            Menu 1
+            </a>
+            <ul role="list" ref={(el) => (navMenuRefs.current[0] = el)}>
+                <li><a href={`javascript:void(0)`} ref={(el) => (navBackLinkRefs.current[0] = el)} onClick={(e) => handleLinkClick(e.target, 0)} toggle-back="true">Submenu 1</a></li>
+                <li><a href={`javascript:void(0)`}>Submenu 2</a></li>
             </ul>
-        )
-    }
-    return (
-        <>
-            <nav>
-                <ul className='nav__menu' role='list'>
-                {itemList.map((item, index) => (
-                    <li className="nav__item" key={index}>
-                            <a href="#" className={`nav__link`} onClick={toggle}>{item.label}</a>
-                            {item.submenu && renderSubmenu(item.submenu)}
-                    </li>
+        </li>
+        <li>
+            <a href={`javascript:void(0)`} onClick={(e) => handleLinkClick(e.target, 1)} toggle-menu="true">
+            Menu 2
+            </a>
+            <ul role="list" ref={(el) => (navMenuRefs.current[1] = el)}>
+                <li><a href={`javascript:void(0)`} ref={(el) => (navBackLinkRefs.current[1] = el)} onClick={(e) => handleLinkClick(e.target, 1)} toggle-back="true">Submenu 3</a></li>
+                <li><a href={`javascript:void(0)`}>Submenu 4</a></li>
+                <li>
+                    <a href={`javascript:void(0)`} onClick={(e) => handleLinkClick(e.target, 3)} toggle-menu="true">Submenu 5</a>
+                    <ul role="list" ref={(el) => (navMenuRefs.current[3] = el)}>
+                        <li><a href={`javascript:void(0)`} ref={(el) => (navBackLinkRefs.current[3] = el)} onClick={(e) => handleLinkClick(e.target, 3)} toggle-back="true">Submenu 1</a></li>
+                        <li><a href={`javascript:void(0)`}>Submenu 2</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </li>
+      </nav>
+    </div>
+  );
+};
 
-                ))}
-                </ul>
-            </nav>
-        </>
-    )
+
+export default function App() {
+    return (<><HeaderNavigation/></>)
 }
